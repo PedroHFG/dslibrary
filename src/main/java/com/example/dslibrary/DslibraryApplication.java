28e2b13f1df0c6b8931bf2bcc6296b867231a90b
PedroHFG <pedrohfidg@gmail.com>
2024-12-13 10:18:54 -0300

    Initial Project

---
+++ src/main/java/com/example/dslibrary/DslibraryApplication.java
@@ -0,0 +1,13 @@
+package com.example.dslibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DslibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DslibraryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/dslibrary/DslibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dslibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DslibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
