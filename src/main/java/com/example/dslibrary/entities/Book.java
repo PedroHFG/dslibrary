7d19db49a1748cf5f40d998d115b73586ca1b4ee
PedroHFG <pedrohfidg@gmail.com>
2024-12-13 11:04:58 -0300

    Created Book entity

---
+++ src/main/java/com/example/dslibrary/entities/Book.java
@@ -0,0 +1,74 @@
+package com.example.dslibrary.entities;
+
+import jakarta.persistence.*;
+
+import java.util.Objects;
+
+@Entity
+@Table(name = "tb_book")
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String author;
+    private Boolean availability;
+
+    public Book() {
+
+    }
+
+    public Book(Long id, String title, String author, Boolean availability) {
+        this.id = id;
+        this.title = title;
+        this.author = author;
+        this.availability = availability;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public Boolean getAvailability() {
+        return availability;
+    }
+
+    public void setAvailability(Boolean availability) {
+        this.availability = availability;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Book book = (Book) o;
+        return Objects.equals(id, book.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+}
